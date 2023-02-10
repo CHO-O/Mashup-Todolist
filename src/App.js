ce8b2a72c3678638ece306aab4b956f6f64784c8
CHO-O <327cjm@gmail.com>
2023-02-10 14:12:51 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components'; //글로벌 스타일 컴포넌트를 만드는 함수
+//페이지의 배경 색상을 설정하기 위해선 body 태그에 css를 적용하면 되는데
+//index.css에서 해도 되지만 styled-components를 이용하고 싶기에 사용하는 것
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef
+  }
+`; 
+
+function App(){
+  return(
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>  
+    </>
   );
 }
 
